f43bc99638c35634503742a7e825cc025a81b5d0
Sofia Paavola <paavola.sofia@gmail.com>
2019-10-02 16:54:30 +0100

    minor changes to code

--- raksu.js
+++ raksu.js
@@ -1,10 +1,7 @@
-let leftDistance = 40; 
+let leftDistance;
 let sofiaOriginalScore = 0;
 let iidaOriginalScore = 0;
-let iidaScore = document.getElementById("iida-score");
-let sofiaScore = document.getElementById("sofia-score")
 const square = document.getElementById("raksu");
-square.style.left = leftDistance + "vw";
 
 document.addEventListener("keyup", event => {
   if (leftDistance === 20 || leftDistance === 60) {
@@ -14,40 +11,43 @@ document.addEventListener("keyup", event => {
   }
 });
 
-const move = (keyCode) => {
-    if (keyCode === 32) {
-        square.style.left = --leftDistance + "vw";
-    } else if (keyCode === 13) {
-        square.style.left = ++leftDistance + "vw";
-    }
-}
+const move = keyCode => {
+  if (keyCode === 32) {
+    square.style.left = --leftDistance + "vw";
+    // if (leftDistance === 30) { 
+    //     square.style.left **= leftDistance + "vw";
+    // }
+  } else if (keyCode === 13) {
+    square.style.left = ++leftDistance + "vw";
+  }
+};
 
 const scoreIncrement = () => {
-    if (leftDistance === 20) {
+  if (leftDistance === 20) {
     iidaOriginalScore++;
-    iidaScore.innerHTML = "Score: " + iidaOriginalScore;
-    } else if (leftDistance === 60) {
+    document.getElementById("iida-score").innerHTML =
+      "Score: " + iidaOriginalScore;
+  } else if (leftDistance === 60) {
     sofiaOriginalScore++;
-    sofiaScore.innerHTML = "Score: " + sofiaOriginalScore;
-    }
-}
+    document.getElementById("sofia-score").innerHTML =
+      "Score: " + sofiaOriginalScore;
+  }
+};
 
-const resetGame  = () => {
-    square.style.left = 40 + "vw";
-    leftDistance = 40;
-}
+const resetGame = () => {
+  leftDistance = 40;
+  square.style.left = leftDistance + "vw";
+};
+
+resetGame(); 
 
 const endGame = () => {
-    if (leftDistance === 20) {
-        alert("Iida won Raksu's affection");
-        scoreIncrement();
-    } else if (leftDistance === 60) {
-        alert("Sofia won Raksu's affection"); 
-        scoreIncrement();
-    }
-    resetGame();
-}
-
-// endGame --> increment appropriate score & alert
-
-// checkIfFinsihed
+  if (leftDistance === 20) {
+    alert("Iida won Raksu's affection");
+    scoreIncrement();
+  } else if (leftDistance === 60) {
+    alert("Sofia won Raksu's affection");
+    scoreIncrement();
+  }
+  resetGame();
+};
